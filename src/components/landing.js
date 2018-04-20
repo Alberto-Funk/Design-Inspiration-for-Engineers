14961081c0dccd859e8d4bf7f6aeae26091fb54a
Cassidy SN Avery <tacolim@gmail.com>
2018-04-20 16:44:55 -0400

    more styles & added github info

--- src/components/landing.js
+++ src/components/landing.js
@@ -11,13 +11,13 @@ const Landing = () => (
       <div className="content__textbox">
         <p>Building a collaborative list of where software engineers can find good design inspiration, tips, and tricks.</p>
         <p>
-          To contribute, please email designinspirationforengineers@gmail.com.
+          To contribute, please email designinspirationforengineers@gmail.com or make a PR on GitHub at https://github.com/tacolim/Design-Inspiration-for-Engineers.
         </p>
       </div>
       <div className="content__links">
-          <Link to='/General' className="content__link"><div className="content__linkDiv">General</div></Link>
-          <Link to='/Colors' className="content__link"><div className="content__linkDiv">Colors</div></Link>
-          <Link to='/Fonts' className="content__link"><div className="content__linkDiv">Fonts</div></Link>
+          <Link to='/General' className="content__link"><div className="content__linkD">General</div></Link>
+          <Link to='/Colors' className="content__link"><div className="content__linkD">Colors</div></Link>
+          <Link to='/Fonts' className="content__link"><div className="content__linkD">Fonts</div></Link>
       </div>
     </div>
   </div>
